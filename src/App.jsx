a4bb931eedcc6b1c709b7e219a3ad253e7f1e73c
Зенченко Евгений <zenchenko_es@outlook.com>
2025-05-19 19:14:15 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+	color: red;
+`;
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-camera-retro fa-lg"></i>
+			<div>123</div>
+		</Div>
+	);
+};
