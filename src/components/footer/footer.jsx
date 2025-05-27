b47a28d8690ad585ee055d0d0d96bc4f185ef309
Зенченко Евгений <zenchenko_es@outlook.com>
2025-05-27 13:27:40 +0700

    Реализовать футер

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
+import { data } from 'react-router-dom';
 import styled from 'styled-components';
 
 //const FooterContainer = styled.div``
-
+// fetch(
+// 			'https://api.openweathermap.org/data/2.5/weather?q=Novokuznetsk&units=metric&lang=ru&appid=8a5a1b66bb40eb3841059525fe2f71a2',
+//     		).then((data) => data.json()).then(console.log);
 const FooterContainer = ({ className }) => {
 	const [city, setCity] = useState('');
 	const [temperature, setTemperature] = useState('');
