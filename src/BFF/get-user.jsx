76694613c9b4f1b34acfdb89bc57364ff26eaa4d
Зенченко Евгений <zenchenko_es@outlook.com>
2025-06-12 00:23:36 +0700

    реализовать страницу регистрации

--- src/BFF/get-user.jsx
+++ src/BFF/get-user.jsx
@@ -1,9 +1,8 @@
 // import { getUsers } from './get-users';
 //запрашивает список всех пользователей и из списка ищет по логину
-export const getUser = async (loginToFind) => {
+export const getUser = async (loginToFind) =>
 	// const users = await getUsers();
 	// return users.find(({ login }) => login === loginToFind);
-	fetch(`http://localhost:3005/users?login=${loginToFind}`)
+	fetch(`http://localhost:3005/users/?login=${loginToFind}`)
 		.then((loadedUser) => loadedUser.json())
 		.then(([loadedUser]) => loadedUser);
-};
