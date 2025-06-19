486c11494c62772c143b739ac30218a825221ad9
Зенченко Евгений <zenchenko_es@outlook.com>
2025-06-19 16:14:27 +0700

    релизовать изменение статьи

--- src/components/pages/post/components/post-content/post-content.jsx
+++ src/components/pages/post/components/post-content/post-content.jsx
@@ -1,35 +1,31 @@
 import styled from 'styled-components';
 import { H2 } from '../../../../h2/h2';
 import { Icon } from '../../../../icon';
+import { SpecialPanel } from '../special-panel/special-panel';
+import { useNavigate } from 'react-router-dom';
 
 const PostContentContainer = ({
 	className,
 	post: { id, title, imageUrl, content, publishedAt },
 }) => {
+	const navigate = useNavigate();
 	return (
 		<div className={className}>
 			<img src={imageUrl} alt={title} />
 			<H2>{title}</H2>
-			<div className="special-panel">
-				<div className="published-at">
-					<Icon
-						id="fa-calendar-o"
-						margin="0 7px 0 0"
-						size="18px"
-						onClick={() => {}}
-					/>
-					{publishedAt}
-				</div>
-				<div className="buttons">
+			<SpecialPanel
+				publishedAt={publishedAt}
+				margin="-20px 0 20px"
+				editButton={
 					<Icon
 						id="fa-pencil-square-o"
 						size="21px"
 						margin="0 10px 0 0"
-						onClick={() => {}}
+						onClick={() => navigate(`/post/${id}/edit`)}
 					/>
-					<Icon id="fa-trash-o" size="21px" onClick={() => {}} />
-				</div>
-			</div>
+				}
+			/>
+
 			<div className="post-text">{content}</div>
 		</div>
 	);
@@ -40,23 +36,9 @@ export const PostContent = styled(PostContentContainer)`
 		float: left;
 		margin: 0 20px 10px 0;
 	}
-	& .special-panel {
-		display: flex;
-		justify-content: space-between;
-		margin: -20px 0 20px;
-	}
-	& .published-at {
-		display: flex;
-		font-size: 18px;
-	}
-	& i {
-		position: relative;
-		top: -1px;
-	}
-	& .buttons {
-		display: flex;
-	}
+
 	& .post-text {
 		font-size: 18px;
+		white-space: pre-line;
 	}
 `;
---
+++ src/components/pages/post/components/post-form/post-form.jsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+
+import { Icon } from '../../../../icon';
+import { Input } from '../../../../input/input';
+import { SpecialPanel } from '../special-panel/special-panel';
+import { useRef } from 'react';
+import { sanizeContent } from './utils';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { savePostAsync } from '../../../../../actions';
+import { useServerRequest } from '../../../../../hooks';
+
+const PostFormContainer = ({
+	className,
+	post: { id, title, imageUrl, content, publishedAt },
+}) => {
+	const imageRef = useRef(null);
+	const titleRef = useRef(null);
+	const contentRef = useRef(null);
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const requestServer = useServerRequest();
+	const onSave = () => {
+		const newImageUrl = imageRef.current.value;
+		const newTitle = titleRef.current.value;
+		const newContent = sanizeContent(contentRef.current.innerText);
+
+		dispatch(
+			savePostAsync(requestServer, {
+				id,
+				imageUrl: newImageUrl,
+				title: newTitle,
+				content: newContent,
+			}),
+		).then(() => navigate(`/post/${id}`));
+	};
+
+	return (
+		<div className={className}>
+			<Input ref={imageRef} defaultValue={imageUrl} placeholder="Изображение..." />
+			<Input ref={titleRef} defaultValue={title} placeholder="Заголовок..." />
+			<SpecialPanel
+				publishedAt={publishedAt}
+				margin="20px 0"
+				editButton={
+					<Icon
+						id="fa-floppy-o"
+						size="21px"
+						margin="0 10px 0 0"
+						onClick={onSave}
+					/>
+				}
+			/>
+
+			<div
+				ref={contentRef}
+				contentEditable={true}
+				suppressContentEditableWarning={true}
+				className="post-text"
+			>
+				{content}
+			</div>
+		</div>
+	);
+};
+
+export const PostForm = styled(PostFormContainer)`
+	& img {
+		float: left;
+		margin: 0 20px 10px 0;
+	}
+
+	& .post-text {
+		font-size: 18px;
+		white-space: pre-line;
+	}
+`;
